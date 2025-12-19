5f8655706d048c6c41bad023a4fba2ebf97db9ed
Adrien Moncho <75078900+Muppssy@users.noreply.github.com>
2025-12-19 10:56:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# < Project name >
+High-level description
+
+Data flow & architecture
+
+Main technologies used and for which purpose
+
+# Running locally
+Instructions to install dependencies, run, build, test
+
+# CI/CD steps
+Short description of each step with their outputs (if any)
---
+++ api/main.py
@@ -0,0 +1,7 @@
+"""
+Ce script doit contenir l'implémentation des endpoints pour les fonctionnalités suivantes :
+- Génération des prédictions pour une date donnée,
+- Récupération des prédictions pour une date donnée,
+- Récupération des prédictions combinées avec des données réelles observées pour une période donnée
+"""
+
